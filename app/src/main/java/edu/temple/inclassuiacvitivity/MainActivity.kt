e8b64d8e4ecdc258d7b140a6aa16274b0ad3efe6
Christopher Brousseau <tul42732@temple.edu>
2024-09-17 11:42:30 -0400

    Modified constraints so views stay in place.

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -13,10 +13,10 @@ class MainActivity : AppCompatActivity() {
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
         /* TODO Step 2: Populate this array with multiples of 5 from 5 - 100*/
-        //val numberArray = Array Of Numbers
+        //val numberArray = IntArray(20) { 5 * (it + 1) }
 
         /* TODO Step 3: Create adapter to display items from array in Spinner */
-        //spinner.adapter = ArrayAdapter...
+        //spinner.adapter = ArrayAdapter..
 
 
         // TODO Step 4: Change TextView's text size to the number selected in the Spinner */
